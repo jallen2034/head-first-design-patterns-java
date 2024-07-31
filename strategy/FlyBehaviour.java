1963a694cf0b58bfe6e9df6e109d217407518299
Jacob Allen <bocajbee@Jacobs-MBP.hitronhub.home>
2024-07-30 19:32:36 -0700

    updated code to demonstrate strategy pattern in java

--- strategy/FlyBehaviour.java
+++ strategy/FlyBehaviour.java
@@ -1,5 +1,8 @@
 package patterns.strategy;
 
+// Interface defining the contract that concrete flying behaviors, such as FlyWithWings, it must adhere to
+// This modular design which allows different flying behaviors to be easily implemented and
+// conditionally used by various kinds of ducks in our Strategy Pattern.
 public interface FlyBehaviour {
-    public void fly();
+    void fly();
 }
--- strategy/MallardDuck.java
+++ strategy/MallardDuck.java
@@ -1,11 +1,16 @@
 package patterns.strategy;
 
+// Concrete subclass of Duck representing a Mallard Duck.
 public class MallardDuck extends Duck {
 
+    // Constructor for MallardDuck, takes a FlyBehaviour instance and passes it to the super class.
     public MallardDuck(FlyBehaviour flyBehaviour) {
         super(flyBehaviour);
     }
 
+    // Overriding performFly to use the fly method defined in the flyBehaviour instance.
+    // This allows the MallardDuck to exhibit its specific flying behavior as defined by the FlyBehaviour.
+    @Override
     public void performFly() {
         flyBehaviour.fly();
     }
