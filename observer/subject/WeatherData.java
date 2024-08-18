8cb948cebe926cd178562cb0d7cb5c7706bdc3b1
Jacob Allen <bocajbee@Jacobs-MacBook-Pro.local>
2024-08-17 22:06:54 -0700

    Added better comments

--- observer/subject/WeatherData.java
+++ observer/subject/WeatherData.java
@@ -1,34 +1,40 @@
 package patterns.observer.subject;
 
 import patterns.observer.observers.Observer;
-
 import java.util.ArrayList;
 import java.util.List;
 
+/* WeatherData acts as the Subject in the Observer pattern, managing a list
+ * of observers and notifying them whenever the weather data changes. */
 public class WeatherData implements Subject {
-    private List<Observer> observers;
+    private List<Observer> observers;  // Holds all registered observers
     private float temperature;
     private float humidity;
     private float pressure;
 
+    // Initializes the list to hold all the observers.
     public WeatherData() {
-        observers = new ArrayList<Observer>();
+        observers = new ArrayList<>();
     }
 
+    // Adds a new observer to the list of observers.
     public void registerObserver(Observer observer) {
         observers.add(observer);
     }
 
+    // Removes an observer from the list of observers.
     public void removeObserver(Observer observer) {
         observers.remove(observer);
     }
 
+    // Notifies all registered observers of any changes to the weather data.
     public void notifyObservers() {
         for (Observer observer : observers) {
             observer.update(temperature, humidity, pressure);
         }
     }
 
+    // Updates the weather data and triggers notification of observers.
     public void setMeasurements(float temperature, float humidity, float pressure) {
         this.temperature = temperature;
         this.humidity = humidity;
@@ -36,5 +42,5 @@ public class WeatherData implements Subject {
         notifyObservers();
     }
 
-    // Todo: other weather data methods will go down here.
+    // TODO: Additional weather data management methods can be added here.
 }
